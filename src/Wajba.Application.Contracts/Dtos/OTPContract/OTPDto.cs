ddc4d3157544d78c3e154e2ec069e71d0249fbec
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2024-12-31 13:38:21 +0200

    changes

--- src/Wajba.Application.Contracts/Dtos/OTPContract/OTPDto.cs
+++ src/Wajba.Application.Contracts/Dtos/OTPContract/OTPDto.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Wajba.Dtos.OTPContract;
 
-namespace Wajba.Dtos.OTPContract
+public class OTPDto : EntityDto<int>
 {
-    public class OTPDto : EntityDto<int>
-    {
-        public OTPType Type { get; set; }
-        public int DigitLimit { get; set; }
-        public int ExpiryTimeInMinutes { get; set; }
-    }
+    public OTPType Type { get; set; }
+    public int DigitLimit { get; set; }
+    public int ExpiryTimeInMinutes { get; set; }
 }
---
+++ src/Wajba.Application/Mapping/OfferMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace Wajba.Mapping;
+
+public class OfferMappingProfile:Profile
+{
+    public OfferMappingProfile()
+    {
+        CreateMap<Offer,OfferDto>()
+            .ForMember(x => x.Id, opt => opt.MapFrom(x => x.Id))
+            .ForMember(x => x.Name, opt => opt.MapFrom(x => x.Name))
+            .ForMember(x => x.Description, opt => opt.MapFrom(x => x.Description))
+            .ForMember(x => x.BranchId, opt => opt.MapFrom(x => x.BranchId))
+            .ForMember(x => x.Status, opt => opt.MapFrom(x => x.status))
+            .ForMember(x => x.StartDate, opt => opt.MapFrom(x => x.StartDate))
+            .ForMember(x => x.EndDate, opt => opt.MapFrom(x => x.EndDate))
+            .ForMember(x => x.Image, opt => opt.MapFrom(x => x.ImageUrl))
+            .ForMember(p=>p.DiscountPercentage, opt => opt.MapFrom(x => x.DiscountPercentage))
+            .ForMember(p=>p.DiscountType, opt => opt.MapFrom(x => x.discountType))
+            .ReverseMap();
+
+    }
+}

98222ee46c9b554d79d602ab84bfee7982e32ae1
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2025-01-01 23:52:14 +0200

    changes

---
+++ src/Wajba.EntityFrameworkCore/Migrations/20250101213116_crewateinitalop.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wajba.Migrations
+{
+    /// <inheritdoc />
+    public partial class crewateinitalop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "oop",
+                table: "Items");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "oop",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}

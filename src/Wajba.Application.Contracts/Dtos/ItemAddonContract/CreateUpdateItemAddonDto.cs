f3e5daab5d4af689c7c0d81967f2a69563f83c1b
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2025-01-05 12:08:12 +0200

    changes

--- src/Wajba.Application.Contracts/Dtos/ItemAddonContract/CreateUpdateItemAddonDto.cs
+++ src/Wajba.Application.Contracts/Dtos/ItemAddonContract/CreateUpdateItemAddonDto.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Wajba.Dtos.ItemAddonContract;
 
-namespace Wajba.Dtos.ItemAddonContract
+public class CreateUpdateItemAddonDto
 {
-    public class CreateUpdateItemAddonDto
-    {
-        public string AddonName { get; set; }
-        public decimal AdditionalPrice { get; set; }
-        public int ItemId { get; set; }
-    }
+    public string AddonName { get; set; }
+    public decimal AdditionalPrice { get; set; }
+    public int ItemId { get; set; }
 }
--- src/Wajba.Application/ItemAddonService/ItemAddonAppService.cs
+++ src/Wajba.Application/ItemAddonService/ItemAddonAppService.cs
@@ -3,10 +3,13 @@
 public class ItemAddonAppService : ApplicationService, IItemAddonAppService
 {
     private readonly IRepository<ItemAddon, int> _itemAddonRepository;
+    private readonly IRepository<Item, int> _itemrepo;
 
-    public ItemAddonAppService(IRepository<ItemAddon, int> itemAddonRepository)
+    public ItemAddonAppService(IRepository<ItemAddon, int> itemAddonRepository,
+        IRepository<Item, int> itemrepo)
     {
         _itemAddonRepository = itemAddonRepository;
+        _itemrepo = itemrepo;
     }
 
     public async Task<List<ItemAddonDto>> GetByItemIdAsync(int itemId)
@@ -21,7 +24,7 @@ public class ItemAddonAppService : ApplicationService, IItemAddonAppService
 
     public async Task<ItemAddonDto> GetByIdAsync(int id)
     {
-        var itemAddon = await _itemAddonRepository.GetAsync(id);
+        ItemAddon itemAddon = await _itemAddonRepository.GetAsync(id);
         if (itemAddon == null)
             throw new EntityNotFoundException(typeof(ItemAddon), id);
         return ObjectMapper.Map<ItemAddon, ItemAddonDto>(itemAddon);
@@ -29,7 +32,15 @@ public class ItemAddonAppService : ApplicationService, IItemAddonAppService
 
     public async Task<ItemAddonDto> CreateAsync(CreateUpdateItemAddonDto input)
     {
-        var itemAddon = ObjectMapper.Map<CreateUpdateItemAddonDto, ItemAddon>(input);
+        Item item = await _itemrepo.GetAsync(input.ItemId);
+        if (item == null)
+            throw new EntityNotFoundException(typeof(Item), input.ItemId);
+        ItemAddon itemAddon = new ItemAddon
+        {
+            ItemId = input.ItemId,
+            AdditionalPrice = input.AdditionalPrice,
+            AddonName = input.AddonName,
+        };
         await _itemAddonRepository.InsertAsync(itemAddon, true);
         return ObjectMapper.Map<ItemAddon, ItemAddonDto>(itemAddon);
     }
@@ -50,9 +61,7 @@ public class ItemAddonAppService : ApplicationService, IItemAddonAppService
     {
         var itemAddon = await _itemAddonRepository.FirstOrDefaultAsync(x => x.ItemId == itemId && x.Id == addonId);
         if (itemAddon == null)
-        {
             throw new EntityNotFoundException($"Addon with ID {addonId} for Item {itemId} not found.");
-        }
-        await _itemAddonRepository.DeleteAsync(itemAddon);
+        await _itemAddonRepository.DeleteAsync(itemAddon, true);
     }
 }

4a3a154e7880c6f4b5bf5ed66fde77662b0b7c85
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2025-01-06 11:40:30 +0200

    chanes

---
+++ src/Wajba.Application/Mapping/FaqMapping.cs
@@ -0,0 +1,16 @@
+﻿namespace Wajba.Mapping;
+
+public class FaqMapping:Profile
+{
+    public FaqMapping()
+    {
+        CreateMap<FAQs, FaqDto>()
+            .ForMember(p => p.Question, opt => opt.MapFrom(p => p.Question))
+            .ForMember(p => p.Answer, opt => opt.MapFrom(p => p.Answer))
+            .ForMember(p => p.Id, opt => opt.MapFrom(p => p.Id));
+        CreateMap<CreateFaqs, FAQs>()
+            .ForMember(p => p.Question, opt => opt.MapFrom(p => p.Question))
+            .ForMember(p => p.Answer, opt => opt.MapFrom(p => p.Answer));
+
+    }
+}

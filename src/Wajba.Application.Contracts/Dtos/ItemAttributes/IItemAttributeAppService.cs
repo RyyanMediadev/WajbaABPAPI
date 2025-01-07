c9c2fc13cfaaca6e69b500db680dfd17760a37a8
ibrahem607 <ibrahem607@gmail.com>
2025-01-07 11:50:24 +0200

    make  itemattributes cruds  customized

--- src/Wajba.Application.Contracts/Dtos/ItemAttributes/IItemAttributeAppService.cs
+++ src/Wajba.Application.Contracts/Dtos/ItemAttributes/IItemAttributeAppService.cs
@@ -1,10 +1,10 @@
 ﻿namespace Wajba.Dtos.ItemAttributes;
 
-public interface IItemAttributeAppService :
-ICrudAppService<
-    ItemAttributeDto,
-    int,
-    PagedAndSortedResultRequestDto,
-    CreateItemAttributeDto>
+public interface IItemAttributeAppService : IApplicationService
 {
+    Task<PagedResultDto<ItemAttributeDto>> GetListAsync(PagedAndSortedResultRequestDto input);
+    Task<ItemAttributeDto> GetAsync(int id);
+    Task<ItemAttributeDto> CreateAsync(CreateItemAttributeDto input);
+    Task<ItemAttributeDto> UpdateAsync(int id, UpdateItemAttributeDto input);
+    Task DeleteAsync(int id);
 }

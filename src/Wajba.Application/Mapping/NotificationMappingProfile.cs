d0516b8d27950f35b62c7b3ffc1e5d81c2491dcf
ibrahem607 <ibrahem607@gmail.com>
2025-01-09 12:17:32 +0200

    add mapping for notification

---
+++ src/Wajba.Application/Mapping/NotificationMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wajba.Dtos.NotificationContract;
+using Wajba.Models.NotificationDomain;
+
+namespace Wajba.Mapping
+{
+    public class NotificationMappingProfile:Profile
+    {
+        public NotificationMappingProfile()
+        {
+            CreateMap<CreateNotificationDto,Notification>().ReverseMap();
+            CreateMap<UpdateNotificationDto, Notification>().ReverseMap();
+            CreateMap<Notification, NotificationDto>().ReverseMap();
+        }
+    }
+}

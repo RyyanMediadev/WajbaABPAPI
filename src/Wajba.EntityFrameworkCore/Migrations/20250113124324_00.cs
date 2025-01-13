e4b4bc75a40b72936f2400ae49878b40aef7d421
Mahmoud <mahmoud@onetelecom.me>
2025-01-13 15:07:48 +0200

    deletecustomer verifyAppuser

---
+++ src/Wajba.EntityFrameworkCore/Migrations/20250113124324_00.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wajba.Migrations
+{
+    /// <inheritdoc />
+    public partial class _00 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+      
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+        }
+    }
+}

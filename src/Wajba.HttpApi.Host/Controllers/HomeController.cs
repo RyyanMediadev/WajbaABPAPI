a7cb0c1e828508d3c02b8dc289d8afb21352ae88
Mahmoud <mahmoud@onetelecom.me>
2025-01-14 13:28:13 +0200

    testhome

--- src/Wajba.HttpApi.Host/Controllers/HomeController.cs
+++ src/Wajba.HttpApi.Host/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿global using Volo.Abp.AspNetCore.Mvc;
 
 namespace Wajba.Controllers;
-[Route("api/[controller]")]
-[ApiController]
+
 public class HomeController : AbpController
 {
-     [HttpGet]
     public ActionResult Index()
     {
         return Redirect("~/swagger");

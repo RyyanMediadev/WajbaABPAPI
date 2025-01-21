01c4c6e1d78506ea0a9a8bd90a26ae3e7a241278
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2025-01-21 10:58:08 +0200

    edit dinetable

--- src/Wajba.Application/DineIntableService/DineinTableAppServices.cs
+++ src/Wajba.Application/DineIntableService/DineinTableAppServices.cs
@@ -1,7 +1,5 @@
 ﻿global using Wajba.Dtos.DineInTableContract;
 global using Wajba.Services.QrCodeServices;
-using Wajba.Models.BranchDomain;
-using Wajba.Models.OfferDomain;
 
 namespace Wajba.DineIntableService;
 
@@ -26,7 +24,7 @@ public class DineinTableAppServices : ApplicationService
             throw new Exception("NotFound branch");
         QrcodeServices qrcodeServices = new QrcodeServices();
         string qrCodeUrl = qrcodeServices.GenerateQrCodeUrl(input.BranchId, input.Name);
-        string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
+        //string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
         var writer = new BarcodeWriterSvg
         {
             Format = BarcodeFormat.QR_CODE,
@@ -49,7 +47,7 @@ public class DineinTableAppServices : ApplicationService
             Name = input.Name,
             Size = input.Size,
             Status = (Status)input.status,
-            QrCode = qrCodeImage,
+            QrCode = url,
             
         };
         Branch branch =await _branchrepo.FindAsync(input.BranchId);
@@ -64,8 +62,7 @@ public class DineinTableAppServices : ApplicationService
             BranchName = branch.Name,
             Phone = branch.Phone,
             Address = branch.Address,
-            QrCode = dineInTable1.QrCode,
-            url = url
+            url = dineInTable1.QrCode
         };
         return diniINDto;
     }
@@ -77,17 +74,8 @@ public class DineinTableAppServices : ApplicationService
         if (dineInTable1 == null)
             throw new Exception("NotFound DineTable");
         QrcodeServices qrcodeServices = new QrcodeServices();
-        dineInTable1.BranchId = dineIntable.BranchId;
-        dineInTable1.Name = dineIntable.Name;
-        dineInTable1.Status = (Status)dineIntable.status;
         string qrCodeUrl = qrcodeServices.GenerateQrCodeUrl(dineIntable.BranchId, dineIntable.Name);
-        string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
-        dineInTable1.QrCode = qrCodeImage;
-        dineInTable1.IsDeleted = false;
-        dineInTable1.Size = dineIntable.Size;
-        dineInTable1.LastModificationTime = DateTime.UtcNow;
-        DineInTable dineInTable3 = await _repository.UpdateAsync(dineInTable1, true);
-        Branch branch = await _branchrepo.FindAsync(dineIntable.BranchId);
+        //string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
         var writer = new BarcodeWriterSvg
         {
             Format = BarcodeFormat.QR_CODE,
@@ -103,7 +91,15 @@ public class DineinTableAppServices : ApplicationService
         byte[] bytes = System.Text.Encoding.UTF8.GetBytes(svgContent);
         using var stream = new MemoryStream(bytes);
         string url = await _imageService.UploadAsync(stream, qrCodeUrl);
-
+        dineInTable1.BranchId = dineIntable.BranchId;
+        dineInTable1.Name = dineIntable.Name;
+        dineInTable1.Status = (Status)dineIntable.status;
+        dineInTable1.QrCode = url;
+        dineInTable1.IsDeleted = false;
+        dineInTable1.Size = dineIntable.Size;
+        dineInTable1.LastModificationTime = DateTime.UtcNow;
+        DineInTable dineInTable3 = await _repository.UpdateAsync(dineInTable1, true);
+        Branch branch = await _branchrepo.FindAsync(dineIntable.BranchId);
         DiniINDto diniINDto = new DiniINDto()
         {
             BranchId = dineInTable3.BranchId,
@@ -111,11 +107,10 @@ public class DineinTableAppServices : ApplicationService
             Status = (int)dineInTable3.Status,
             Name = dineInTable3.Name,
             Size = (byte)dineInTable3.Size,
-            QrCode = dineInTable3.QrCode,
             Address = branch.Address,
             Phone = branch.Phone,
             BranchName = branch.Name,
-            url=url
+            url=dineInTable3.QrCode
         };
         return diniINDto;
     }
@@ -137,7 +132,6 @@ public class DineinTableAppServices : ApplicationService
         List<DiniINDto> diniINDtos = new List<DiniINDto>();
         foreach(var i in dineInTables)
         {
-
             DiniINDto diniINDto = new DiniINDto()
             {
                 Address = i.Branch.Address,
@@ -148,28 +142,8 @@ public class DineinTableAppServices : ApplicationService
                 BranchId = i.BranchId,
                 Size = (byte)i.Size,
                 Status = (int)i.Status,
-                QrCode = i.QrCode,
-                url = ""
+                url = i.QrCode
             };
-            var writer = new BarcodeWriterSvg
-            {
-                Format = BarcodeFormat.QR_CODE,
-                Options = new EncodingOptions
-                {
-                    Width = 300,  // Width of the QR code
-                    Height = 300, // Height of the QR code
-                    Margin = 1    // Margin around the QR code
-                }
-            };
-            QrcodeServices qrcodeServices = new QrcodeServices();
-            string qrCodeUrl = qrcodeServices.GenerateQrCodeUrl(i.BranchId, i.Name);
-            string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
-            var svgImage = writer.Write(qrCodeUrl);
-            var svgContent = svgImage.Content;
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(svgContent);
-            using var stream = new MemoryStream(bytes);
-            string url = await _imageService.UploadAsync(stream, qrCodeUrl);
-            diniINDto.url = url;
             diniINDtos.Add(diniINDto);
         }
         return new PagedResultDto<DiniINDto>(
@@ -183,29 +157,9 @@ public class DineinTableAppServices : ApplicationService
         if (dine == null)
             throw new Exception("Not Found");
         Branch branch = await _branchrepo.GetAsync(id);
-        QrcodeServices qrcodeServices = new QrcodeServices();
-        string qrCodeUrl = qrcodeServices.GenerateQrCodeUrl(branch.Id, dine.Name);
-        string qrCodeImage = qrcodeServices.GenerateQrCodeImage(qrCodeUrl);
-        var writer = new BarcodeWriterSvg
-        {
-            Format = BarcodeFormat.QR_CODE,
-            Options = new EncodingOptions
-            {
-                Width = 300,  // Width of the QR code
-                Height = 300, // Height of the QR code
-                Margin = 1    // Margin around the QR code
-            }
-        };
-        var svgImage = writer.Write(qrCodeUrl);
-        var svgContent = svgImage.Content;
-        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(svgContent);
-        using var stream = new MemoryStream(bytes);
-        string url = await _imageService.UploadAsync(stream, qrCodeUrl);
-
         DiniINDto diniINDto = new DiniINDto()
         {
             Name = dine.Name,
-            QrCode = dine.QrCode,
             Status = (int)dine.Status,
             BranchId = dine.BranchId,
             Id = dine.Id,
@@ -213,7 +167,7 @@ public class DineinTableAppServices : ApplicationService
             BranchName = branch.Name,
             Phone = branch.Phone,
             Address = branch.Address,
-            url=url
+            url = dine.QrCode
         };
         return diniINDto;
     }

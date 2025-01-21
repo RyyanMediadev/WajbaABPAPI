aecd17e45c61a1179c79d5c1577aa03a4873377d
Mahmoud <mahmoud@onetelecom.me>
2025-01-21 15:49:38 +0200

    UserManagmentV2

---
+++ src/Wajba.Application/Mapping/WajbaUserMapping.cs
@@ -0,0 +1,13 @@
+﻿using Wajba.Dtos.CustomerContract;
+using Wajba.Models.WajbaUserDomain;
+
+namespace Wajba.Mapping;
+
+public class WajbaUserMapping : Profile
+{
+    public WajbaUserMapping()
+    {
+        CreateMap<WajbaUser, WajbaUserDto>();
+        CreateMap<WajbaUserDto, WajbaUser>();
+    }
+}

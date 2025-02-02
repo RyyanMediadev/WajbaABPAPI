4044f18a4f34a38b5406c0f43f383a2f77b99fff
AhmedAbdalazeem2007 <ahmedmon997@gmail.com>
2025-02-02 17:03:47 +0200

    changes

--- src/Wajba.Application/CartServices/CartAppService.cs
+++ src/Wajba.Application/CartServices/CartAppService.cs
@@ -90,10 +90,13 @@ public class CartAppService : ApplicationService
             DiscountAmount = cart.DiscountAmount,
             ServiceFee = cart.ServiceFee,
             Note = cart.Note,
+            SubTotal=cart.SubTotal,
             Items = cart.CartItems.Select(p => new CartItemDto()
             {
                 Notes = p.Notes,
                 ItemId = p.ItemId,
+                ItemName=p.ItemName,
+                price=p.price,
                 Quantity = p.Quantity,
                 Addons = p.SelectedAddons.Select(l => new CartItemAddonDto()
                 {
@@ -121,12 +124,12 @@ public class CartAppService : ApplicationService
     public async Task<CartDto> CreateAsync(int customerid, List<CartItemDto> cartItemDtos)
     {
         Cart cart = await _CartRepository.FirstOrDefaultAsync(p => p.CustomerId == customerid);
-        if (cart != null)
-            foreach (var i in cart.CartItems)
-                await _cartitemrepo.HardDeleteAsync(i, true);
-        foreach (var i in await _cartitemrepo.ToListAsync())
-            if (i.CartId == cart.Id)
-                await _cartitemrepo.HardDeleteAsync(i, true); 
+        //if (cart != null)
+            //foreach (var i in cart.CartItems)
+            //    await _cartitemrepo.HardDeleteAsync(i, true);
+        //foreach (var i in await _cartitemrepo.ToListAsync())
+        //    if (i.CartId == cart.Id)
+        //        await _cartitemrepo.HardDeleteAsync(i, true);
         if (cart == null)
         {
             cart = new Cart()
@@ -142,6 +145,20 @@ public class CartAppService : ApplicationService
             };
             cart = await _CartRepository.InsertAsync(cart, true);
         }
+        else
+        {
+            cart.TotalAmount = 0;
+            cart.SubTotal = 0;
+            cart.DeliveryFee = 0;
+            cart.ServiceFee = 0;
+            cart.CartItems = new List<CartItem>();
+            cart.Note = "";
+            cart.DiscountAmount = 0;
+            cart.voucherCode = 0;
+            await _CartRepository.UpdateAsync(cart,true);
+        };
+        bool isfound = true;
+        cart.CartItems = new List<CartItem>();
         foreach (var i in cartItemDtos)
         {
             Item item = await _itemrepo.FirstOrDefaultAsync(p => p.Id == i.ItemId);
@@ -150,6 +167,7 @@ public class CartAppService : ApplicationService
             CartItem cartItem = await _cartitemrepo.FirstOrDefaultAsync(p => p.ItemId == i.ItemId && p.CartId == cart.Id);
             if (cartItem == null)
             {
+                isfound = false;
                 cartItem = new CartItem()
                 {
                     CartId = cart.Id,
@@ -164,50 +182,63 @@ public class CartAppService : ApplicationService
                     SelectedVariations = new List<CartItemVariation>(),
                     SelectedExtras = new List<CartItemExtra>()
                 };
-                foreach (var j in i.Addons)
-                {
-                    ItemAddon itemAddon = await _itemaddonrepo.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
-                    if (itemAddon == null)
-                        throw new Exception("Invalid data");
-                    cartItem.SelectedAddons.Add(new CartItemAddon()
-                    {
-                        AdditionalPrice = itemAddon.AdditionalPrice,
-                        AddonId = itemAddon.Id,
-                        AddonName = itemAddon.AddonName,
-                    });
-                    cart.SubTotal += itemAddon.AdditionalPrice;
-                }
-                foreach (var j in i.Extras)
-                {
-                    ItemExtra itemExtra = await _itemextrarepo.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
-                    if (itemExtra == null)
-                        throw new Exception("Invalid data");
-                    cartItem.SelectedExtras.Add(new CartItemExtra()
-                    {
-                        AdditionalPrice = itemExtra.AdditionalPrice,
-                        ExtraName = itemExtra.Name,
-                        ExtraId = itemExtra.Id,
-
-                    });
-                    cart.SubTotal += itemExtra.AdditionalPrice;
-                }
-                foreach (var j in i.Variations)
-                {
-                    ItemVariation itemVariation = await _itemvariationrepo.WithDetailsAsync(p => p.ItemAttributes).Result.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
-                    if (itemVariation == null)
-                        throw new Exception("Invalid data");
-                    cartItem.SelectedVariations.Add(new CartItemVariation()
-                    {
-                        VariationName = itemVariation.Name,
-                        VariationId = itemVariation.Id,
-                        AdditionalPrice = itemVariation.AdditionalPrice,
-                        Attributename = itemVariation.ItemAttributes.Name
-                    });
-                    cart.SubTotal += itemVariation.AdditionalPrice;
-                }
             }
-            CartItem cartItem1 = await _cartitemrepo.InsertAsync(cartItem, true);
-            cart.CartItems.Add(cartItem1);
+            else
+            {
+                cartItem.price = item.Price;
+                cartItem.Quantity = i.Quantity;
+                cartItem.Notes = i.Notes;
+                cartItem.ImgUrl = item.ImageUrl;
+                cartItem.SelectedAddons = new List<CartItemAddon>();
+                cartItem.SelectedExtras = new List<CartItemExtra>();
+                cartItem.SelectedVariations = new List<CartItemVariation>();
+            }
+            foreach (var j in i.Addons)
+            {
+                ItemAddon itemAddon = await _itemaddonrepo.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
+                if (itemAddon == null)
+                    throw new Exception("Invalid data");
+                cartItem.SelectedAddons.Add(new CartItemAddon()
+                {
+                    AdditionalPrice = itemAddon.AdditionalPrice,
+                    AddonId = itemAddon.Id,
+                    AddonName = itemAddon.AddonName,
+                });
+                cart.SubTotal += itemAddon.AdditionalPrice;
+            }
+            foreach (var j in i.Extras)
+            {
+                ItemExtra itemExtra = await _itemextrarepo.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
+                if (itemExtra == null)
+                    throw new Exception("Invalid data");
+                cartItem.SelectedExtras.Add(new CartItemExtra()
+                {
+                    AdditionalPrice = itemExtra.AdditionalPrice,
+                    ExtraName = itemExtra.Name,
+                    ExtraId = itemExtra.Id,
+
+                });
+                cart.SubTotal += itemExtra.AdditionalPrice;
+            }
+            foreach (var j in i.Variations)
+            {
+                ItemVariation itemVariation = await _itemvariationrepo.WithDetailsAsync(p => p.ItemAttributes).Result.FirstOrDefaultAsync(p => p.Id == j.Id && p.ItemId == item.Id);
+                if (itemVariation == null)
+                    throw new Exception("Invalid data");
+                cartItem.SelectedVariations.Add(new CartItemVariation()
+                {
+                    VariationName = itemVariation.Name,
+                    VariationId = itemVariation.Id,
+                    AdditionalPrice = itemVariation.AdditionalPrice,
+                    Attributename = itemVariation.ItemAttributes.Name
+                });
+                cart.SubTotal += itemVariation.AdditionalPrice;
+            }
+
+            if (!isfound)
+                await _cartitemrepo.InsertAsync(cartItem, true);
+            else await _cartitemrepo.UpdateAsync(cartItem, true);
+            cart.CartItems.Add(cartItem);
         }
         cart.SubTotal += cart.CartItems.Sum(p => p.Quantity * p.price);
         cart.TotalAmount = cart.SubTotal + cart.ServiceFee + cart.DeliveryFee + cart.DiscountAmount;
